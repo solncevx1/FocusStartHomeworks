303b60daa5a377a8cb15dcad2b889a833bb61cc2
solncevx1 <solncevx1@gmail.com>
2020-10-26 17:56:49 +0700

    Initial Commit

---
+++ Homework__1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework__1
+//
+//  Created by Максим Солнцев on 10/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
